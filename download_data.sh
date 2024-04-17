80750dff71e96d5d4cee54e63048f8aeb33ff6e7
John Arevalo <johnarevalo@gmail.com>
2024-04-17 14:06:48 -0400

    download script receives command line arg

--- download_data.sh
+++ download_data.sh
@@ -1,17 +1,26 @@
-pert="orf"
+if [ $# -ne 1 ]; then
+  echo "Error: Missing argument."
+  echo "Usage: $0 (orf|crispr|compound)"
+  exit 1
+fi
+
+# Validate argument value
+pert="$1"
+if [[ ! "$pert" =~ ^(orf|crispr|compound)$ ]]; then
+  echo "Error: Invalid argument. Please provide 'orf', 'crispr', or 'compound'."
+  echo "Usage: $0 (orf|crispr|compound)"
+  exit 1
+fi
 
 configfile="inputs/$pert.json"
 BASEPATH="s3://cellpainting-gallery/cpg0016-jump"
-# To download all of the sources in the JUMP dataset
-# sources=`aws s3 ls --no-sign-request "$BASEPATH/" | awk '{print substr($2, 1, length($2)-1)}'`
-# sources="source_2 source_3 source_6 source_8 source_10"
 
 readarray -t sources < <(jq -r '.sources[]' "$configfile")
 
 mkdir -p inputs/metadata
 for source_id in "${sources[@]}";
 do
-    aws s3 cp --recursive --no-sign-request "$BASEPATH/$source_id/workspace/profiles" inputs/$source_id/workspace/profiles
+    aws s3 sync --no-sign-request "$BASEPATH/$source_id/workspace/profiles" inputs/$source_id/workspace/profiles
 done
 
 wget https://github.com/jump-cellpainting/datasets/blob/main/metadata/plate.csv.gz?raw=true -O inputs/metadata/plate.csv.gz
