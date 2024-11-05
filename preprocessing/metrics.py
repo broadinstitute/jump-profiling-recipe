e6cb7d490632cbcf5bdf8c5e3e773582ce53dca2
Shantanu Singh <shsingh@broadinstitute.org>
2024-11-04 20:15:21 -0500

    Generalize

--- preprocessing/metrics.py
+++ preprocessing/metrics.py
@@ -1,12 +1,15 @@
 import copairs.map as copairs
 import pandas as pd
-
+import numpy as np
 from preprocessing.io import split_parquet
 from preprocessing.metadata import NEGCON_CODES
 
 
 def _index(meta, plate_types, ignore_codes=None, include_codes=None):
     """Select samples to be used in mAP computation"""
+    # if nothing is specified, use all samples
+    if plate_types is None and ignore_codes is None:
+        return np.ones(len(meta), dtype=bool)
     index = meta["Metadata_PlateType"].isin(plate_types)
     index &= meta["Metadata_pert_type"] != "poscon"
     valid_cmpd = meta.loc[index, "Metadata_JCP2022"].value_counts()
@@ -55,7 +58,18 @@ def average_precision_negcon(parquet_path, ap_path, plate_types):
     result.reset_index(drop=True).to_parquet(ap_path)
 
 
-def average_precision_nonrep(parquet_path, ap_path, plate_types):
+def average_precision_nonrep(
+    parquet_path,
+    ap_path,
+    plate_types,
+    ap_params={
+        "pos_sameby": ["Metadata_JCP2022"],
+        "pos_diffby": [],
+        "neg_sameby": ["Metadata_Plate"],
+        "neg_diffby": ["Metadata_JCP2022"],
+        "batch_size": 20000,
+    },
+):
     meta, vals, _ = split_parquet(parquet_path)
     ix = _index(meta, plate_types, ignore_codes=NEGCON_CODES)
     meta = meta[ix].copy()
@@ -63,19 +77,22 @@ def average_precision_nonrep(parquet_path, ap_path, plate_types):
     result = copairs.average_precision(
         meta,
         vals,
-        pos_sameby=["Metadata_JCP2022"],
-        pos_diffby=[],
-        neg_sameby=["Metadata_Plate"],
-        neg_diffby=["Metadata_JCP2022"],
-        batch_size=20000,
+        **ap_params,
     )
     result.reset_index(drop=True).to_parquet(ap_path)
 
 
-def mean_average_precision(ap_path, map_path, threshold=0.05):
+def mean_average_precision(
+    ap_path,
+    map_path,
+    map_params={
+        "threshold": 0.05,
+        "sameby": "Metadata_JCP2022",
+        "null_size": 10000,
+        "seed": 0,
+    },
+):
     ap_scores = pd.read_parquet(ap_path)
 
-    map_scores = copairs.mean_average_precision(
-        ap_scores, "Metadata_JCP2022", threshold=threshold, null_size=10000, seed=0
-    )
+    map_scores = copairs.mean_average_precision(ap_scores, **map_params)
     map_scores.to_parquet(map_path)
